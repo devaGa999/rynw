6bf95893ca00505bb16736f55d65d29f633bfd1a
devaGa999 <144148669+devaGa999@users.noreply.github.com>
2023-10-01 16:19:24 +0400

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ from flask import Flask, request
 from flask_restful import Api, Resource
 import requests
 
+load_dotenv()
 wh = os.getenv('WEBHOOK')
 
+print(wh)
 app = Flask(__name__)
 api = Api(app)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ips = {}
 
-load_dotenv()
+
 
 with open("config.json", "r") as f:
     config = json.load(f)
